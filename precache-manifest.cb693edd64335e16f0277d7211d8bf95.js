93e71ac5bda86704b173b9fca81d44c8629fc8bc
lvl <mamad.za69@gmail.com>
2020-03-18 21:39:19 +0330

    Updates

---
+++ precache-manifest.cb693edd64335e16f0277d7211d8bf95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a9b50d6b299de88b019de222b831c7f",
+    "url": "/reactjs-test/index.html"
+  },
+  {
+    "revision": "758991eb9bca2e47bd19",
+    "url": "/reactjs-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "20be536cc25509b3b14e",
+    "url": "/reactjs-test/static/js/2.82ef4516.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/reactjs-test/static/js/2.82ef4516.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "758991eb9bca2e47bd19",
+    "url": "/reactjs-test/static/js/main.44561624.chunk.js"
+  },
+  {
+    "revision": "46df68860395e90a9647",
+    "url": "/reactjs-test/static/js/runtime-main.82bb83d6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactjs-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
